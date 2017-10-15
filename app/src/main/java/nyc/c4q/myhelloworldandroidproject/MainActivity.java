43cf2401dbb59266f9ec6ae106ec1f5c1580c22c
Francisco Andrade <xaviandrade14@gmail.com>
2017-10-15 13:35:38 -0400

    Screenshoot deleted

--- app/src/main/java/nyc/c4q/myhelloworldandroidproject/MainActivity.java
+++ app/src/main/java/nyc/c4q/myhelloworldandroidproject/MainActivity.java
@@ -2,12 +2,65 @@ package nyc.c4q.myhelloworldandroidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.d(TAG, " running code in the onCreate() method " );
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        Log.d(TAG, " running code in the onStart() method " );
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, " running code in the onResume() method " );
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, " running code in the onPause() method " );
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, " running code in the onStop() method " );
+
+        onDestroy();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, " running code in the onDestroy() method " );
+
+
     }
 }
